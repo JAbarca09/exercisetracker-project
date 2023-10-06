433baf8f43bf3fffd77a389c7741f96739bb55e0
Jesse Abarca <JesseAbarca888@gmail.com>
2023-10-05 22:53:45 -0700

    Connect to the mongoDB db hosted on Atlas

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('DB connection successful!'));
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
