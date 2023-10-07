ccacd426f89a4cd32e31c967611850a268fa73f6
Jesse Abarca <JesseAbarca888@gmail.com>
2023-10-06 21:23:50 -0700

    Connect userRoutes to index.js and use bodyParser

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config({ path: './sample.env' });
 // require('dotenv').config();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
@@ -21,6 +24,8 @@ const DB = process.env.DATABASE.replace(
 mongoose
   .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('DB connection successful!'));
+
+app.use('/api', userRoutes);
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
