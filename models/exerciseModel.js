f3941b6523526e673f64d058d27812413cb319bf
Jesse Abarca <JesseAbarca888@gmail.com>
2023-10-09 00:22:39 -0700

    Remove username from the exerciseSchema

--- models/exerciseModel.js
+++ models/exerciseModel.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
 const exerciseSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
   description: {
     type: String,
     required: true,
