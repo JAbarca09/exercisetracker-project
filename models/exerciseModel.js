5b2b6a461dcebde72ebeee1b90d8580f831726d3
Jesse Abarca <JesseAbarca888@gmail.com>
2023-10-10 21:28:28 -0700

    Update exerciseModel to have userId

--- models/exerciseModel.js
+++ models/exerciseModel.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const exerciseSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
   description: {
     type: String,
     required: true,
