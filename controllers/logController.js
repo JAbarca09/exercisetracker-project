a6620c4546dce869d4239a219192c6c1fb93a657
Jesse Abarca <JesseAbarca888@gmail.com>
2023-10-26 22:49:25 -0700

    Create a log for the user when it does not exist in getUserLogs

--- controllers/logController.js
+++ controllers/logController.js
@@ -1,10 +1,12 @@
 const User = require('../models/userModel');
+const Exercise = require('../models/exerciseModel');
 const Log = require('../models/logModel');
 
 exports.getUserLogs = async (req, res) => {
-  const userId = req.params[':_id'];
+  const userId = req.params['_id'];
 
   const existingUser = await User.findById(userId);
+  console.log(existingUser);
 
   if (!existingUser) {
     return res.status(404).json({ message: 'User not found' });
@@ -12,10 +14,29 @@ exports.getUserLogs = async (req, res) => {
   // Found a user now check if they have a log
   const existingLog = await Log.findOne({ username: existingUser.username });
   if (!existingLog) {
-    return res.status(404).json({ message: 'User has no log!' });
+    // TODO create a log
+    const exercises = await Exercise.find({ username: existingUser.username });
+    const exercise = exercises.map((element) => {
+      return {
+        description: element.description,
+        duration: element.duration,
+        date: element.date,
+      };
+    });
+    console.log(exercise);
+    // Create one if needed
+    const log = new Log({
+      _id: userId,
+      username: existingUser.username,
+      count: exercise.length,
+      log: exercise,
+    });
+
+    await log.save();
+    return res.status(200).json(log);
   }
-  // Create one if needed
-  // Update if needed
+  // TODO check if the log needs to be updated
+  // TODO Update the log
 
   return res.status(200).json({ message: 'Hello World' });
 };
