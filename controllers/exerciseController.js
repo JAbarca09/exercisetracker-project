5384111c5a609f08507d89a000350ca15527f8a5
Jesse Abarca <JesseAbarca888@gmail.com>
2023-11-03 18:13:13 -0700

    Save date objects to db

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -2,7 +2,6 @@ const User = require('../models/userModel');
 const Exercise = require('../models/exerciseModel');
 // Response
 // {"_id":"652382314c3ec20832c9fd1e","username":"Rekby","date":"Tue Aug 03 2021","duration":10,"description":"Heyo"}
-
 const isValidDate = (date) => {
   return !isNaN(new Date(date));
 };
@@ -18,19 +17,21 @@ exports.createExercise = async (req, res) => {
     }
 
     let formattedDate;
+    let DB_DATE;
 
     if (isValidDate(date)) {
+      DB_DATE = new Date(date);
       formattedDate = new Date(date).toDateString();
     } else {
-      const today = new Date();
-      formattedDate = today.toDateString();
+      DB_DATE = new Date(); // today's date
+      formattedDate = DB_DATE.toDateString();
     }
 
     const newExercise = new Exercise({
       username: existingUser.username,
       description,
       duration,
-      date: formattedDate,
+      date: DB_DATE,
     });
 
     await newExercise.save();
