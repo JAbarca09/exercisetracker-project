26763e7dbd33ac206c8e5aceac65124ea8c6bce2
Jesse Abarca <JesseAbarca888@gmail.com>
2023-11-04 21:33:27 -0700

    Implement from, to, and limit for log query

--- controllers/logController.js
+++ controllers/logController.js
@@ -2,8 +2,14 @@ const User = require('../models/userModel');
 const Exercise = require('../models/exerciseModel');
 const Log = require('../models/logModel');
 
+// Example query 
+// http://localhost:3000/api/users/6520e2304efe5e4f2ac92687/logs?from=2020-01-01&to=2021-01-01&limit=1
+// FIXME save dates as date objects to the db but responses are toDateString() instead!
 exports.getUserLogs = async (req, res) => {
   const userId = req.params['_id'];
+  const from = req.query.from;
+  const to = req.query.to;
+  const limit = req.query.limit;
 
   const existingUser = await User.findById(userId);
 
@@ -15,7 +21,22 @@ exports.getUserLogs = async (req, res) => {
 
   if (!existingLog) {
     // If no log exists, create a new log
-    const exercises = await Exercise.find({ username: existingUser.username });
+    let exercisesQuery = { username: existingUser.username };
+
+    if (from && to) {
+      exercisesQuery.date = { $gte: from, $lte: to };
+    } else if (from) {
+      exercisesQuery.date = { $gte: from };
+    } else if (to) {
+      exercisesQuery.date = { $lte: to };
+    }
+
+    let exercises = await Exercise.find(exercisesQuery);
+
+    if (limit) {
+      exercises = exercises.slice(0, parseInt(limit));
+    }
+
     const exercise = exercises.map((element) => {
       return {
         description: element.description,
@@ -34,7 +55,22 @@ exports.getUserLogs = async (req, res) => {
     await log.save();
     return res.status(200).json(log);
   } else {
-    const exercises = await Exercise.find({ username: existingUser.username });
+    let exercisesQuery = { username: existingUser.username };
+
+    if (from && to) {
+      exercisesQuery.date = { $gte: from, $lte: to };
+    } else if (from) {
+      exercisesQuery.date = { $gte: from };
+    } else if (to) {
+      exercisesQuery.date = { $lte: to };
+    }
+
+    let exercises = await Exercise.find(exercisesQuery);
+
+    if (limit) {
+      exercises = exercises.slice(0, parseInt(limit));
+    }
+
     const exerciseCount = exercises.length;
 
     if (exerciseCount !== existingLog.count) {
